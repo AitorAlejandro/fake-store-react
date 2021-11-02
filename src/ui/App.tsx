313c2e496b3ff777dc2f9c7ceeca318ff93718d7
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-03 00:14:19 +0100

    App.test requirements fix

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,6 +6,7 @@ import 'normalize-css';
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
+        <h1>Fake Store React</h1>
         <Switch>
           <Route path="/">
             <Home />
