f1a828b3c3545b6c9415bf5d493f31cd2571ace6
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-15 23:23:15 +0100

    Home is using HomeLayout

---
+++ src/ui/layouts/HomeLayout.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+interface HomeLayoutProps {
+  children: React.ReactNode;
+}
+
+export const HomeLayout: React.FC<HomeLayoutProps> = (props) => {
+  const { children } = props;
+
+  return (
+    <div>
+      <header style={{ backgroundColor: "#eaeaea" }}>
+        <h1>Fake Store Home</h1>
+      </header>
+      <div className="container">{children}</div>
+      <footer style={{ backgroundColor: "#eaeaea" }}>
+        <h2>Store Footer</h2>
+      </footer>
+    </div>
+  );
+};
